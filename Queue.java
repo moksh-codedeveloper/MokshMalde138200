c61f1ba72783000ad22dd50f4b387a749c2193ad
MokshMlade <maldemoksh437@gmail.com>
2023-12-14 19:34:29 +0530

    A Queue file

---
+++ Queue.java
@@ -0,0 +1,66 @@
+public class Queue {
+    static class QueueY {
+        static int arr[];
+        static int size;
+        static int rear = -1;
+
+        QueueY(int n) {
+            arr = new int[size];
+            this.size = n;
+
+        }
+
+        public static Boolean isEmpty() {
+            return rear == -1;
+        }
+
+        // enqueue
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("full queue");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+
+        }
+
+        // dequeue
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("empty");
+                return -1;
+            }
+
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty");
+                return -1;
+            }
+
+            return arr[0];
+        }
+    }
+
+    public static void main(String[] args) {
+        QueueY q = new QueueY(5);
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
