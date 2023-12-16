53871e6c4832e5bf2a381c4b5d8b960f56a7a993
MokshMlade <maldemoksh437@gmail.com>
2023-12-16 17:48:28 +0530

    A new question for queue

--- Queue3.java
+++ Queue3.java
@@ -2,8 +2,48 @@ import java.util.*;
 import java.util.Queue;
 
 public class Queue3 {
+    static class Queue {
+        static Stack<Integer> s1 = new Stack<Integer>();
+        static Stack<Integer> s2 = new Stack<Integer>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return s1.pop();
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return s1.peek();
+
+        }
+    }
+
     public static void main(String args[]) {
-        Queue<Integer> q = new LinkedList<Integer>();
+        Queue q = new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
