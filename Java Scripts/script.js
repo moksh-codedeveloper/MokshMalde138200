f6f8f0a7691029a1fee6f4595d38856af7e8c4c1
MokshMlade <maldemoksh437@gmail.com>
2023-12-19 14:11:00 +0530

    A new folder in new branch for devlopement

--- Java Scripts/script.js
+++ Java Scripts/script.js
@@ -2,3 +2,10 @@ const age = 24;
 const name = "Tony Stark";
 name = "Man";
 console.log(name , age);
+
+const student = {
+    fullName : "Moksh Malde",
+    age : 24,
+    cgpa : 9.9,
+    isPass : true,
+};
